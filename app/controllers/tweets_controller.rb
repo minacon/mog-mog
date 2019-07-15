5d93efa188dd2b2e0a7c467a9a9baae39e2fe25e
minacon <minacon655@gmail.com>
2019-07-15 20:11:37 +0900

    tweet/new.html.erb作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,19 @@
 class TweetsController < ApplicationController
   
   def index
-    
+    @tweet = Tweet.all
   end
 
   def new
     
+  end
+
+  def create
+    Tweet.create(title: tweet_params[:title], image: tweet_params[:image], text: tweet_params[:text])
+  end
+
+  private
+  def tweet_params
+    params.permit(:image, :text, :title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_14_084108) do
+ActiveRecord::Schema.define(version: 2019_07_15_065036) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
