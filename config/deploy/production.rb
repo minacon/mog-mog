09fe8d642b5cf948dddeceaf524daedba597fb1a
minacon <minacon655@gmail.com>
2019-08-21 12:21:52 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -52,11 +52,11 @@
 # server "example.com",
 #   user: "user_name",
 #   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
 server '18.179.232.75', user: 'ec2-user', roles: %w{app db web}
+  ssh_options: {
+    user: "ec2-user", # overrides user setting above
+    keys: %w(~/.ssh/mog.pem),
+    forward_agent: false,
+    # auth_methods: %w(publickey password)
+    # password: "please use keys"
+  }
