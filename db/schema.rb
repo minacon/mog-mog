e4a85413c255167d5116c35c74b4b04a3305300a
minacon <minacon655@gmail.com>
2019-09-20 11:58:36 +0900

    like_counts　defolt"0"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_081041) do
+ActiveRecord::Schema.define(version: 2019_09_20_025449) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "tweet_id"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2019_08_25_081041) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.integer "user_id"
-    t.integer "likes_count"
+    t.integer "like_counts", default: 0
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
