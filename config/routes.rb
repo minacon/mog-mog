f975e3062f558ec8e5a496148375427fe96a26e0
minacon <minacon655@gmail.com>
2019-09-21 16:14:33 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets do
+    collection do
+      get 'search'
+    end
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
